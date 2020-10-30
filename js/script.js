912fcd2e1216f20681f44542c77f7adac3c34f5a
asidarevich <asidarevich@webim-team.ru>
2020-10-30 07:06:44 +0300

    Better

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-$(document).ready(function () {
-    $('.about_section').show();
-});
+var SELECTED_BORDER_STYLE = "5px solid #781a2d";
+var DEFAULT_BORDER_STYLE = "2px solid lightgray";
+
+var DEFAULT_BACKGROUND_COLOR = "rgba(54, 25, 25, .1)"
+var SELECTED_BACKGROUND_COLOR = "rgba(120, 26, 45, .3)"
+
+function unmark_all() {
+    $('.about_section').css('border', DEFAULT_BORDER_STYLE);
+    $('.experience_section').css('border', DEFAULT_BORDER_STYLE);
+    $('.projects_section').css('border', DEFAULT_BORDER_STYLE);
+    $('.achievements_section').css('border', DEFAULT_BORDER_STYLE);
+
+    $('.about_section').css('background-color', DEFAULT_BACKGROUND_COLOR);
+    $('.experience_section').css('background-color', DEFAULT_BACKGROUND_COLOR);
+    $('.projects_section').css('background-color', DEFAULT_BACKGROUND_COLOR);
+    $('.achievements_section').css('background-color', DEFAULT_BACKGROUND_COLOR);
+}
 
 $('.fixed_head .fixed_center_container .about').click(function () {
-    $('.about_section').show();
-    $('.experience_section').hide();
-    $('.projects_section').hide();
-    $('.achievements_section').hide();
+    unmark_all();
+    $('.about_section').css('border', SELECTED_BORDER_STYLE);
+    $('.about_section').css('background-color', SELECTED_BACKGROUND_COLOR);
 });
 
 $('.fixed_head .fixed_center_container .experience').click(function () {
-    $('.about_section').hide();
-    $('.experience_section').show();
-    $('.projects_section').hide();
-    $('.achievements_section').hide();
+    unmark_all();
+    $('.experience_section').css('border', SELECTED_BORDER_STYLE);
+    $('.experience_section').css('background-color', SELECTED_BACKGROUND_COLOR);
 });
 
 $('.fixed_head .fixed_center_container .projects').click(function () {
-    $('.about_section').hide();
-    $('.experience_section').hide();
-    $('.projects_section').show();
-    $('.achievements_section').hide();
-}) ;
+    unmark_all();
+    $('.projects_section').css('border', SELECTED_BORDER_STYLE);
+    $('.projects_section').css('background-color', SELECTED_BACKGROUND_COLOR);
+});
 
 $('.fixed_head .fixed_center_container .achievements').click(function () {
-    $('.about_section').hide();
-    $('.experience_section').hide();
-    $('.projects_section').hide();
-    $('.achievements_section').show();
-}) ;
+    unmark_all();
+    $('.achievements_section').css('border', SELECTED_BORDER_STYLE);
+    $('.achievements_section').css('background-color', SELECTED_BACKGROUND_COLOR);
+});
